7c43f1995ca4959afa210e21561b48c8dcae2cb1
Youngjae Lee <77099686+YoungJaeLee-github@users.noreply.github.com>
2021-01-28 01:26:58 +0900

    Add files via upload

---
+++ BOJ1008/BOJ1008/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  BOJ1008
+//
+//  Created by 이영재 on 2021/01/27.
+//
+
+import Foundation
+
+let input = readLine()!.components(separatedBy: " ").map{Double($0)!}
+print(input[0]/input[1])
