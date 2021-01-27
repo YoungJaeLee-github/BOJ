9229b5586b41947fe27165dde634fbbbe6369b44
Youngjae Lee <77099686+YoungJaeLee-github@users.noreply.github.com>
2021-01-28 01:26:41 +0900

    Add files via upload

---
+++ BOJ10998/BOJ10998/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  BOJ10998
+//
+//  Created by 이영재 on 2021/01/27.
+//
+
+import Foundation
+
+if let input = readLine(){
+    let parse = input.split(separator: " ")
+    print(Int(parse[0])! * Int(parse[1])!)
+}
