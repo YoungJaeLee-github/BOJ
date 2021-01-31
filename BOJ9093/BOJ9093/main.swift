707adb5cf0fc78fe0b0c136042bd6dd9c288fa4e
Youngjae Lee <77099686+YoungJaeLee-github@users.noreply.github.com>
2021-01-31 16:53:14 +0900

    Add files via upload

---
+++ BOJ9093/BOJ9093/main.swift
@@ -0,0 +1,60 @@
+//
+//  main.swift
+//  BOJ9093
+//
+//  Created by 이영재 on 2021/01/31.
+//
+
+var testCase = Int(readLine()!)!
+var stack: Array<Character> = []
+var size: Int = 0
+
+func push(n: Character) -> Void {
+    stack.append(n)
+    size += 1
+}
+
+func pop() -> Character {
+    if empty() {
+        return " "
+    } else {
+        let result = stack[size - 1]
+        stack.remove(at: size - 1)
+        size -= 1
+        return result
+    }
+}
+
+func top () -> Character {
+    if empty() {
+        return " "
+    } else {
+        return stack[size - 1]
+    }
+}
+
+func empty() -> Bool {
+    return size == 0 ? true : false
+}
+
+func count() -> Int {
+    return size
+}
+
+while testCase != 0 {
+    var input = readLine()!
+    input.append("\n")
+    
+    for i in input {
+        if i == " " || i == "\n" {
+            while count() != 0 {
+                print("\(pop())", separator: "", terminator: "")
+            }
+            if i != "\n"{ print(i, separator: "", terminator: "") }
+        } else {
+            push(n: i)
+        }
+    }
+    print()
+    testCase -= 1
+}
