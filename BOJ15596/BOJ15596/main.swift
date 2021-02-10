7f544e05bac64fc0127480e19afc3d91fcbf8ffe
YoungjaeLee <77099686+YoungJaeLee-github@users.noreply.github.com>
2021-02-10 15:46:48 +0900

    정수 N개의 합(언어 제한)
    백준 Swift 언어 제한, Java로 제출

---
+++ BOJ15596/BOJ15596/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  BOJ15596
+//
+//  Created by 이영재 on 2021/02/10.
+//
+//
+func sum(a: Array<Int>) -> Int {
+    var ans: Int = 0
+    for i in a {
+        ans += i
+    }
+    return ans
+}
